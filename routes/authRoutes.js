01552a14703808b200c19fd2a5eac4890f71d90e
Bahaayaad <46580002+Bahaayaad@users.noreply.github.com>
2024-02-18 21:15:49 +0300

    profile controller

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,5 +7,4 @@ router.post('/signup', verifyTokenAuth, authController.signup);
 router.post('/login', authController.login);
 router.get('/logout', authController.logout);
 router.get('/validate', verifyTokenAuth, authController.tokenValidate)
-router.get('/profile/:id', verifyTokenAuth, authController.profileView)
 module.exports = router;
